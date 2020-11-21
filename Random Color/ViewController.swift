30518ab8e363c0700b38e4d7938562e1c8ee0b08
Jude Wilson <judewilson2024@gmail.com>
2020-11-20 20:29:59 -0500

    Initial Commit

---
+++ Random Color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Color
+//
+//  Created by Jude Wilson on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
